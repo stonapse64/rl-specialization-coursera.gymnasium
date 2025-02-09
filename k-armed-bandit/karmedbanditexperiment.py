34876167f6b387f19569e04cd5fc56b297515a36
stonapse64 <stonapse@outlook.de>
2025-02-09 10:41:05 +0100

    Refactoring karmedbanditexperiment

--- k-armed-bandit/karmedbanditexperiment.py
+++ k-armed-bandit/karmedbanditexperiment.py
@@ -12,11 +12,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import trange
 # import local libs
+# TODO refactor
 from mab_bandit import Bandit
 from mab_player import Player
 
 
-class ExperimentMAB:
+class BanditCompetition:
     """
     A class to represent an experiment for a multi-armed bandit problem.
 
@@ -26,8 +27,8 @@ class ExperimentMAB:
         Configuration for the experiment runs and timesteps.
     bandit_config : dict
         Configuration for the bandit.
-    player_configs : dict
-        Configuration for the players.
+    agent_configs : dict
+        Configuration for the agents.
     system_load : float
         The system load for distributing the processes.
 
@@ -51,32 +52,32 @@ class ExperimentMAB:
                      "q_drift_mean": 0.0, "q_drift_std_dev": 0.01,
                      "stationary": False, "random_seed": None}
 
-    player_configs = {
+    agent_configs = {
         "sample average": {"epsilon": 0.1, "alpha": 0., "random_seed": None},
         "constant stepsize": {"epsilon": 0.1, "alpha": 0.1,
                               "random_seed": None}
     }
 
-    For fun and comparison: you can add a cheater to your player
+    For fun and comparison: you can add a cheater to your agent
     configuration that would always draw the arm with the maximum
     reward. epsilon and alpha can be arbitrary as they won't be used.
 
-    player_configs = {
+    agent_configs = {
     "cheater": {"epsilon": 1., "alpha": 1., "random_seed": None},
-    "other player": {...},
+    "other agent": {...},
     ...,
     }
 
     Example usage
     -------------
 
-    experiment = ExperimentMAB(casino_config, bandit_config, player_configs, system_load=0.8)
+    experiment = ExperimentMAB(casino_config, bandit_config, agent_configs, system_load=0.8)
     rewards, loss, optimal_actions = experiment.run_experiment()
     experiment.plot_results(rewards, loss, optimal_actions, save_plots=True)
 
     """
 
-    def __init__(self, casino_config, bandit_config, player_configs, system_load=0.8):
+    def __init__(self, competition_config, bandit_config, agent_configs, system_load=0.8):
         """
         Initializes the ExperimentMAB class with given configurations.
 
@@ -86,14 +87,16 @@ class ExperimentMAB:
             Configuration for the experiment runs and timesteps.
         bandit_config : dict
             Configuration for the bandit.
-        player_configs : dict
-            Configuration for the players.
+        agent_configs : dict
+            Configuration for the agents.
         system_load : float, optional
             The system load for distributing the processes (default is 0.8).
         """
-        self.casino_config = casino_config
+        # TODO refactor 
+        self.competition_config = competition_config
         self.bandit_config = bandit_config
-        self.player_configs = player_configs
+        self.agent_configs = agent_configs
+
         self.system_load = system_load
         self.average_rewards_all_runs, self.average_loss_all_runs, self.optimal_actions_taken_all_runs = {}, {}, {}
         self.batches, self.batch_size = self.size_batches()
@@ -109,9 +112,12 @@ class ExperimentMAB:
         """
         _num_cores = os.cpu_count()
         _batches = int(_num_cores * self.system_load)
-        _batch_size = self.casino_config["runs"] // _batches
+        _batch_size = self.competition_config["runs"] // _batches
         _effective_runs = _batches * _batch_size
-        self.casino_config["runs"] = _effective_runs
+        
+        # TODO>DONE: refactor
+        self.competition_config["runs"] = _effective_runs
+
         print(f'{_effective_runs} total runs from {_batches} batches with {_batch_size} runs each.')
         return _batches, _batch_size
 
@@ -122,25 +128,27 @@ class ExperimentMAB:
         Returns
         -------
         tuple
-            A tuple containing average rewards and percentage of optimal actions for all players across all runs.
+            A tuple containing average rewards and percentage of optimal actions for all agents across all runs.
         """
         with ProcessPoolExecutor(max_workers=self.batches) as executor:
             results = list(executor.map(self.run_batches, [self.batch_size for _ in range(self.batches)]))
 
-        cumulative_rewards = {player: 0 for player in self.player_configs}
-        cumulative_loss = {player: 0 for player in self.player_configs}
-        cumulative_optimal_actions = {player: 0 for player in self.player_configs}
+        # TODO>DONE: refactor
+        cumulative_rewards = {agent: 0 for agent in self.agent_configs}
+        cumulative_loss = {agent: 0 for agent in self.agent_configs}
+        cumulative_optimal_actions = {agent: 0 for agent in self.agent_configs}
 
         for result in results:
             rewards, loss, optimal_actions = result
-            for player in self.player_configs:
-                cumulative_rewards[player] += rewards[player]
-                cumulative_loss[player] += loss[player]
-                cumulative_optimal_actions[player] += optimal_actions[player]
+            for agent in self.agent_configs:
+                cumulative_rewards[agent] += rewards[agent]
+                cumulative_loss[agent] += loss[agent]
+                cumulative_optimal_actions[agent] += optimal_actions[agent]
 
-        self.average_rewards_all_runs = {player: cumulative_rewards[player] / self.batches for player in self.player_configs}
-        self.average_loss_all_runs = {player: cumulative_loss[player] / self.batches for player in self.player_configs}
-        self.optimal_actions_taken_all_runs = {player: cumulative_optimal_actions[player] / self.batches for player in self.player_configs}
+        # TODO>DONE: refactor
+        self.average_rewards_all_runs = {agent: cumulative_rewards[agent] / self.batches for agent in self.agent_configs}
+        self.average_loss_all_runs = {agent: cumulative_loss[agent] / self.batches for agent in self.agent_configs}
+        self.optimal_actions_taken_all_runs = {agent: cumulative_optimal_actions[agent] / self.batches for agent in self.agent_configs}
 
         return self.average_rewards_all_runs, self.average_loss_all_runs, self.optimal_actions_taken_all_runs
 
@@ -158,44 +166,47 @@ class ExperimentMAB:
         tuple
             A tuple containing average rewards and percentage of optimal actions per timestep across all runs.
         """
-        runs, timesteps = _batch_size, self.casino_config["timesteps"]
-        players, rewards_run, loss_run, optimal_actions, average_rewards_all_runs, average_loss_all_runs, percentage_optimal_actions = {}, {}, {}, {}, {}, {}, {}
+        runs, timesteps = _batch_size, self.competition_config["timesteps"]
+        agents, rewards_run, loss_run, optimal_actions, average_rewards_all_runs, average_loss_all_runs, percentage_optimal_actions = {}, {}, {}, {}, {}, {}, {}
 
-        for player in self.player_configs:
-            average_rewards_all_runs[player] = np.zeros(timesteps)
-            average_loss_all_runs[player] = np.zeros(timesteps)
-            optimal_actions[player] = np.zeros(timesteps)
-            percentage_optimal_actions[player] = np.zeros(timesteps)
+        for agent in self.agent_configs:
+            average_rewards_all_runs[agent] = np.zeros(timesteps)
+            average_loss_all_runs[agent] = np.zeros(timesteps)
+            optimal_actions[agent] = np.zeros(timesteps)
+            percentage_optimal_actions[agent] = np.zeros(timesteps)
 
         for _run in trange(runs, desc="Runs"):
+            # TODO refactor 
             bandit = Bandit(self.bandit_config)
-            for player in self.player_configs:
-                players[player] = Player(self.player_configs[player], bandit)
-                rewards_run[player] = np.zeros(timesteps)
-                loss_run[player] = np.zeros (timesteps)
+            for agent in self.agent_configs:
+                agents[agent] = Player(self.agent_configs[agent], bandit)
+                rewards_run[agent] = np.zeros(timesteps)
+                loss_run[agent] = np.zeros (timesteps)
 
             for timestep in range(timesteps):
-                for player in players:
+                for agent in agents:
 
-                    if player == "cheater":
+                    # TODO refactor
+                    if agent == "cheater":
                         action, reward = np.argmax(bandit.arms), np.max(bandit.arms)
                     else:
-                        action, reward = players[player].move()
+                        action, reward = agents[agent].move()
 
-                    rewards_run[player][timestep] = reward
-                    optimal_actions[player][timestep] += 1 if action == np.argmax(bandit.arms) else 0
-                    loss_run[player][timestep] = np.max(bandit.arms) - reward
+                    rewards_run[agent][timestep] = reward
+                    optimal_actions[agent][timestep] += 1 if action == np.argmax(bandit.arms) else 0
+                    loss_run[agent][timestep] = np.max(bandit.arms) - reward
 
+                    # TODO refactor 
                     bandit.drift()
 
-            for player in players:
-                average_rewards_all_runs[player] += (1 / (runs + 1)) * (rewards_run[player] - average_rewards_all_runs[player])
-                average_loss_all_runs[player] += (1 / (runs + 1)) * (
-                            loss_run[player] - average_loss_all_runs[
-                        player])
+            for agent in agents:
+                average_rewards_all_runs[agent] += (1 / (runs + 1)) * (rewards_run[agent] - average_rewards_all_runs[agent])
+                average_loss_all_runs[agent] += (1 / (runs + 1)) * (
+                            loss_run[agent] - average_loss_all_runs[
+                        agent])
 
-        for player in players:
-            percentage_optimal_actions[player] = optimal_actions[player] / runs
+        for agent in agents:
+            percentage_optimal_actions[agent] = optimal_actions[agent] / runs
 
         return average_rewards_all_runs, average_loss_all_runs, percentage_optimal_actions
 
@@ -221,9 +232,9 @@ class ExperimentMAB:
         # loss
         plt.figure(figsize=(10, 6))
         title = ""
-        for i, player in enumerate(self.player_configs):
-            plt.plot(average_loss_all_runs[player][:], label=f'{player} player', linestyle="--" if player == "cheater" else None, alpha=self.alpha(i))
-            title += f'{player} player: {self.player_configs[player]}\n'
+        for i, agent in enumerate(self.agent_configs):
+            plt.plot(average_loss_all_runs[agent][:], label=f'{agent} agent', linestyle="--" if agent == "cheater" else None, alpha=self.alpha(i))
+            title += f'{agent} agent: {self.agent_configs[agent]}\n'
         plt.title(f'Average loss per timestep over {runs} runs\n{title}')
         plt.xlabel('Timesteps')
         plt.ylabel('Average Loss')
@@ -239,9 +250,9 @@ class ExperimentMAB:
         # rewards
         plt.figure(figsize=(10, 6))
         title = ""
-        for i, player in enumerate(self.player_configs):
-            plt.plot(average_rewards_all_runs[player][:], label=f'{player} player', linestyle="--" if player == "cheater" else None, alpha=self.alpha(i))
-            title += f'{player} player: {self.player_configs[player]}\n'
+        for i, agent in enumerate(self.agent_configs):
+            plt.plot(average_rewards_all_runs[agent][:], label=f'{agent} agent', linestyle="--" if agent == "cheater" else None, alpha=self.alpha(i))
+            title += f'{agent} agent: {self.agent_configs[agent]}\n'
         plt.title(f'Average rewards per timestep over {runs} runs\n{title}')
         plt.xlabel('Timesteps')
         plt.ylabel('Average Reward')
@@ -257,9 +268,9 @@ class ExperimentMAB:
         # rewards cum.
         plt.figure(figsize=(10, 6))
         title = ""
-        for i, player in enumerate(self.player_configs):
-            plt.plot(np.cumsum(average_rewards_all_runs[player]), label=f'{player} player', linestyle="--" if player == "cheater" else None, alpha=self.alpha(i))
-            title += f'{player} player: {self.player_configs[player]}\n'
+        for i, agent in enumerate(self.agent_configs):
+            plt.plot(np.cumsum(average_rewards_all_runs[agent]), label=f'{agent} agent', linestyle="--" if agent == "cheater" else None, alpha=self.alpha(i))
+            title += f'{agent} agent: {self.agent_configs[agent]}\n'
         plt.title(f'Cumulative Average rewards per timestep over {runs} runs\n{title}')
         plt.xlabel('Timesteps')
         plt.ylabel('Cumulative Average Reward')
@@ -276,9 +287,9 @@ class ExperimentMAB:
         # optimal actions
         plt.figure(figsize=(10, 6))
         title = ""
-        for i, player in enumerate(self.player_configs):
-            plt.plot(percentage_optimal_actions[player][:], label=f'{player} player', linestyle="--" if player == "cheater" else None, alpha=self.alpha(i))
-            title += f'{player} player: {self.player_configs[player]}\n'
+        for i, agent in enumerate(self.agent_configs):
+            plt.plot(percentage_optimal_actions[agent][:], label=f'{agent} agent', linestyle="--" if agent == "cheater" else None, alpha=self.alpha(i))
+            title += f'{agent} agent: {self.agent_configs[agent]}\n'
         plt.title(f'Percentage optimal actions per timestep over {runs} runs\n{title}')
         plt.xlabel('Timesteps')
         plt.ylabel('% Optimal Actions')
@@ -300,7 +311,7 @@ class ExperimentMAB:
         :param i: layer number, generated by code
         :return: alpha value
         """
-        x = len(self.player_configs) -i
+        x = len(self.agent_configs) -i
         a, b, c, d = 0.8, 0.5, 0.7,  0.2
         alpha = a * np.exp(-b * x**c) + d
         return alpha
